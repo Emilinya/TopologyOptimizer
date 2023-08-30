e90eb68197cb214220f469a64e91a37965b3d07a
Emilie <emilid2103@gmail.com>
2023-08-30 16:30:31 +0200

    removed * import + misc improvements

--- topopt/topopt.py
+++ topopt/topopt.py
@@ -1,10 +1,10 @@
-from dolfin import *
-from dolfin_adjoint import *
+import dolfin as df
+import dolfin_adjoint as dfa
 import numpy as np
 from scipy import io
 import ufl
 
-set_log_level(LogLevel.ERROR)
+df.set_log_level(df.LogLevel.ERROR)
 
 import Hs_regularization as Hs_reg
 from ipopt_solver import IPOPTSolver, IPOPTProblem
@@ -22,21 +22,18 @@ except ImportError:
 
 
 # Define the boundary condition on velocity
-class InflowOutflow(UserExpression):
-    def init(self, delta):
-        self.delta = delta
+class InflowOutflow(dfa.UserExpression):
+    def __init__(self, **kwargs):
+        super(InflowOutflow, self).__init__(kwargs)
+        self.domain_size = kwargs["domain_size"]
 
     def eval(self, values, x):
-        if not hasattr(self, "delta"):
-            print("Tried to eval InflowOutflow before calling init!")
-            exit(1)
-
         values[1] = 0.0
         values[0] = 0.0
         l = 1.0 / 6.0
         gbar = 1.0
 
-        if x[0] == 0.0 or x[0] == self.delta:
+        if x[0] == 0.0 or x[0] == self.domain_size[0]:
             if (1.0 / 4 - l / 2) < x[1] < (1.0 / 4 + l / 2):
                 t = x[1] - 1.0 / 4
                 values[0] = gbar * (1 - (2 * t / l) ** 2)
@@ -49,130 +46,121 @@ class InflowOutflow(UserExpression):
 
 
 # Define the boundary condition on pressure
-class PressureB(SubDomain):
+class PressureB(df.SubDomain):
     def inside(self, x, on_boundary):
-        return near(x[0], (0.0)) and near(x[1], (0.0))
+        return df.near(x[0], (0.0)) and df.near(x[1], (0.0))
 
 
 def alpha(rho):
     """Inverse permeability as a function of rho, equation (40)"""
-    equation = alphabar * (
+    equation = alpha_max * (
         -1.0 / 16 * rho**4 + 3.0 / 8 * rho**2 - 0.5 * rho + 3.0 / 16
     )
-    return conditional(
-        gt(rho, 1.0), 0.0, conditional(gt(rho, -1.0), equation, -1.0 * alphabar * rho)
+    return df.conditional(
+        df.gt(rho, 1.0),
+        0.0,
+        df.conditional(df.gt(rho, -1.0), equation, -1.0 * alpha_max * rho),
     )
 
 
 def forward(rho, flowBC, pressureBC):
     """Solve the forward problem for a given fluid distribution rho(x)."""
-    w = Function(W)
-    (u, p) = TrialFunctions(W)
-    (v, q) = TestFunctions(W)
+    w = dfa.Function(solution_space)
+    (u, p) = df.TrialFunctions(solution_space)
+    (v, q) = df.TestFunctions(solution_space)
 
     F = (
-        alpha(rho) * inner(u, v) * dx
-        + inner(grad(u), grad(v)) * dx
-        + inner(grad(p), v) * dx
-        + inner(div(u), q) * dx
-    )
+        alpha(rho) * df.inner(u, v)
+        + df.inner(df.grad(u), df.grad(v))
+        + df.inner(df.grad(p), v)
+        + df.inner(df.div(u), q)
+    ) * df.dx
     bc = [
-        DirichletBC(W.sub(0), flowBC, "on_boundary"),
-        DirichletBC(W.sub(1), Constant(0.0), pressureBC, method="pointwise"),
+        dfa.DirichletBC(solution_space.sub(0), flowBC, "on_boundary"),
+        dfa.DirichletBC(
+            solution_space.sub(1), dfa.Constant(0.0), pressureBC, method="pointwise"
+        ),
     ]
-    solve(lhs(F) == rhs(F), w, bcs=bc)
+    dfa.solve(df.lhs(F) == df.rhs(F), w, bcs=bc)
 
     return w
 
 
-def save_control(x0, controls_file, eta, params, J=None):
-    rho = preprocessing.dof_to_control(x0)
-    rho.rename("density", "density")
-    print("objective function value J", J(rho))
-    controls_file << rho
-
-    filename = "output/data/design_" + f"{N=}_{eta=}" + ".mat"
+def save_control(x0, eta, params):
+    filename = f"output/data/design_{N=}_{eta=}.mat"
     io.savemat(filename, mdict={"data": x0, "w": params["w"], "h": params["h"]})
 
 
 if __name__ == "__main__":
     # turn off redundant output in parallel
-    parameters["std_out_all_processes"] = False
+    df.parameters["std_out_all_processes"] = False
 
-    mu = Constant(1.0)  # viscosity
-    alphabar = 2.5 * mu / (0.01**2)  # parameter for \alpha
+    # define constatns
+    viscosity = dfa.Constant(1.0)
+    alpha_max = 2.5 * viscosity / (0.01**2)
 
-    params = {
-        "N": 40,
-        "w": 1.5,
-        "h": 1.0,
-    }
-    N, delta = params["N"], params["w"]
-
-    V = (1.0 * delta) / 3  # want the fluid to occupy 1/3 of the domain
-    mesh = Mesh(
-        RectangleMesh(
-            MPI.comm_world, Point(0.0, 0.0), Point(delta, 1.0), int(delta * N), N
+    # define domain
+    N = 40
+    width = 1.5
+    height = 1
+    volume_fraction = 1 / 3
+    V = width * height * volume_fraction
+    mesh = dfa.Mesh(
+        dfa.RectangleMesh(
+            df.MPI.comm_world,
+            df.Point(0.0, 0.0),
+            df.Point(width, height),
+            int(width * N),
+            int(height * N),
         )
     )
 
-    controls_file = File("output/extra/final_controls_" + str(N) + ".pvd")
+    # define solution space
+    control_space = df.FunctionSpace(mesh, "DG", 0)
+    velocity_space = df.VectorElement("CG", mesh.ufl_cell(), 2)
+    pressure_space = df.FiniteElement("CG", mesh.ufl_cell(), 1)
+    solution_space = df.FunctionSpace(mesh, velocity_space * pressure_space)
 
-    U_h = VectorElement("CG", mesh.ufl_cell(), 2)
-    P_h = FiniteElement("CG", mesh.ufl_cell(), 1)
-    W = FunctionSpace(mesh, U_h * P_h)  # mixed Taylor-Hood function space
-
-    B = FunctionSpace(mesh, "DG", 0)  # control function space
-    b = Function(B)
-    k = len(b.vector()[:])
-    b.vector()[:] = range(k)
-
-    flowBC = InflowOutflow(degree=2)
-    flowBC.init(delta)
+    # define boundary conditions
+    flowBC = InflowOutflow(degree=2, domain_size=(width, height))
     pressureBC = PressureB()
 
-    x0 = (2.0 * V / delta - 1) * np.ones(int(k / 2))
+    # create initial conditions
+    k = int(width * N) * int(height * N) * 2
+    x0 = (2.0 * volume_fraction - 1) * np.ones(int(k / 2))
 
     # preprocessing class which contains dof_to_control-mapping
-    sigma = 7.0 / 16
-    preprocessing = Preprocessing(N, B)
-
+    preprocessing = Preprocessing(N, control_space)
     rho = preprocessing.dof_to_control(x0)
 
     # get reduced objective function: rho --> j(rho)
-    set_working_tape(Tape())
+    dfa.set_working_tape(dfa.Tape())
     w = forward(rho, flowBC, pressureBC)
-    (u, p) = split(w)
-
-    controls = File("output/extra/control_iterations_guess" + str(N) + ".pvd")
-    allctrls = File("output/extra/allcontrols_" + str(N) + ".pvd")
-    rho_viz = Function(B, name="ControlVisualisation")
-
-    def eval_cb(j, rho):
-        rho_viz.assign(rho)
-        controls << rho_viz
-        allctrls << rho_viz
+    (u, p) = df.split(w)
 
     # objective function
-    J = assemble(
-        0.5 * inner(alpha(rho) * u, u) * dx + 0.5 * mu * inner(grad(u), grad(u)) * dx
+    J = dfa.assemble(
+        0.5 * df.inner(alpha(rho) * u, u) * df.dx
+        + 0.5 * viscosity * df.inner(df.grad(u), df.grad(u)) * df.dx
     )
     # penalty term in objective function
-    J2 = assemble(
-        ufl.Max(rho - 1.0, 0.0) ** 2 * dx + ufl.Max(-rho - 1.0, 0.0) ** 2 * dx
+    J2 = dfa.assemble(
+        ufl.Max(rho - 1.0, 0.0) ** 2 * df.dx + ufl.Max(-rho - 1.0, 0.0) ** 2 * df.dx
     )
-    #
-    Js = [J, J2]
-    m = Control(rho)
 
-    Jeval = ReducedFunctional(J, m)
+    Js = [J, J2]
+    m = dfa.Control(rho)
+    Jeval = dfa.ReducedFunctional(J, m)
     # Note: the evaluation of the gradient can be speed up since the adjoint solve requires no pde solve
     # (see Appendix A.4)
 
     # constraints
-    v = 1.0 / V * assemble((0.5 * (rho + 1)) * dx) - 1.0  # volume constraint
-    s = assemble(1.0 / delta * (rho * rho - 1.0) * dx)  # spherical constraint
-    constraints = [ReducedFunctional(v, m), ReducedFunctional(s, m)]
+    volume_constraint = 1.0 / V * dfa.assemble((0.5 * (rho + 1)) * df.dx) - 1.0
+    spherical_constraint = dfa.assemble(volume_fraction / V * (rho * rho - 1.0) * df.dx)
+    constraints = [
+        dfa.ReducedFunctional(volume_constraint, m),
+        dfa.ReducedFunctional(spherical_constraint, m),
+    ]
     # scaling of constraints for Ipopt
     scaling_constraints = [1.0, 1.0]
 
@@ -189,26 +177,23 @@ if __name__ == "__main__":
     for j, (weight, eta, bound) in enumerate(zip(weights, etas, bounds)):
         # update inner product
         # consider L2-mass-matrix + weighting * Hs-matrix
-        inner_product_matrix = Hs_reg.AssembleHs(N, delta, sigma).get_matrix(weight)
-
-        scaling_Js = [1.0, eta]
+        sigma = 7.0 / 16
+        inner_product_matrix = Hs_reg.AssembleHs(N, width, sigma).get_matrix(weight)
 
         # for performance reasons we first add J and J2 and hand the sum over to the IPOPT solver
-        J_ = 0
-        for i in range(len(Js)):
-            J_ += Js[i] * scaling_Js[i]
-        Jhat = [ReducedFunctional(J_, m, eval_cb_post=eval_cb)]
+        J_ = Js[0] + eta * Js[1]
+        Jhat = [dfa.ReducedFunctional(J_, m)]
         # Note: the evaluation of the gradient can be speed up since the adjoint solve requires no pde solve
         # (see Appendix A.4)
 
         if j != 0:
             # move x0 onto sphere
-            x0 = preprocessing.move_onto_sphere(x0, V, delta)
+            x0 = preprocessing.move_onto_sphere(x0, V, width)
 
         # solve optimization problem
         problem = IPOPTProblem(
             Jhat,
-            scaling_Js,
+            [1.0, eta],
             constraints,
             scaling_constraints,
             bound,
@@ -219,4 +204,4 @@ if __name__ == "__main__":
         ipopt = IPOPTSolver(problem)
 
         x0 = ipopt.solve(x0)
-        save_control(x0, controls_file, eta, params, J=Jeval)
+        save_control(x0, eta, {"w": width, "h": height})
